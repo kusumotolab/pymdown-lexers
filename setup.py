f98b63d2c2005a37d42a4d1160caba9813d5df37
shinsuke-mat <shinsuke-mat@users.noreply.github.com>
2024-09-06 22:42:15 +0900

    init

---
+++ setup.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+"""Setup pymdown-lexers."""
+from setuptools import setup, find_packages
+
+entry_points = '''
+[pygments.lexers]
+lexp==pymdown_lexp:LambdaLexer
+'''
+
+setup(
+    name='pymdown-lexers',
+    version='1.0.0',
+    description='Pygments lexer package for PyMdown.',
+    author='Shin',
+    url='https://github.com/kusumotolab/pymdown-lexers',
+    packages=find_packages(),
+    entry_points=entry_points,
+    install_requires=[
+        'Pygments>=2.0.1'
+    ],
+    zip_safe=True,
+    license='MIT License',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Text Processing :: Filters',
+        'Topic :: Text Processing :: Markup :: HTML'
+    ]
+)
---
+++ src/lexp.py
@@ -0,0 +1,22 @@
+from pygments.lexer import RegexLexer
+from pygments.token import *
+
+class LexpLexer(RegexLexer):
+    name = 'lexp'
+    aliases = ['lexp']
+    filenames = ['*.lexp']
+
+    tokens = {
+        'root': [
+            (r'[a-z]+', Name),
+            (r'[A-Z]+', Name),
+            (r'\s+', Whitespace),
+            (r'[0-9]+', Number),
+            (r'λ', Keyword),
+            (r'->', Keyword),
+            (r'//.*?$', Comment.Singleline),
+            (r'\(', Punctuation),
+            (r'\)', Punctuation),
+            (r'\.', Operator),
+        ]
+    }
